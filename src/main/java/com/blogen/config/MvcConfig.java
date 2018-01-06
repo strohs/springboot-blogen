57f957b215a584178d6d313174e9e516e7c7b065
cliff <strohs1@gmail.com>
2018-01-06 15:21:38 -0500

    initial base config with all dependencies listed

---
+++ src/main/java/com/blogen/config/MvcConfig.java
@@ -0,0 +1,81 @@
+package com.blogen.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * Configure the view used by this project.
+ *
+ * @author Cliff
+ * 1/6/18
+ */
+@Configuration
+@EnableWebMvc
+public class MvcConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext( ApplicationContext applicationContext ) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setContentType("text/html");
+        resolver.setOrder( Ordered.LOWEST_PRECEDENCE - 5 );
+        return resolver;
+    }
+
+    @Bean
+    public TemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        //TODO enable thymeleaf security tags
+//        Set<IDialect> dialects = new HashSet<>();
+//        dialects.add( new SpringSecurityDialect() );
+//        engine.setAdditionalDialects( dialects );
+
+        engine.setEnableSpringELCompiler(true);
+        engine.setTemplateResolver( templateResolver() );
+        return engine;
+    }
+
+    private ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext( applicationContext );
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode( TemplateMode.HTML );
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable( true );
+        return resolver;
+    }
+}
