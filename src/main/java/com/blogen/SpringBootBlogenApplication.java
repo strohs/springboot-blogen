b0ad5c765c783da66655245c77d29702ce49eb16
cliff <strohs1@gmail.com>
2018-01-06 20:12:25 -0500

    initial base config with all dependencies listed

--- src/main/java/com/blogen/SpringBootBlogenApplication.java
+++ src/main/java/com/blogen/SpringBootBlogenApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootBlogenApplication {
 
+	//todo spring-boot-web project has security config example plus h2-console security config
+    //todo /SpringProjects/sg-spring-core-advanced/spring-core-spring-mvc has example security config and H2 config
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBlogenApplication.class, args);
 	}
