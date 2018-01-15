81b2794bcff13fa97ebf527335d7cfd614c677ba
CliffS <strohs1@gmail.com>
2018-01-15 17:37:12 -0500

    initial thymeleaf layout of posts.html with test data

--- src/main/java/com/blogen/controllers/PostController.java
+++ src/main/java/com/blogen/controllers/PostController.java
@@ -2,22 +2,27 @@ package com.blogen.controllers;
 
 import com.blogen.commands.CategoryCommand;
 import com.blogen.commands.PageCommand;
+import com.blogen.commands.PostCommand;
 import com.blogen.commands.UserCommand;
 import com.blogen.services.CategoryService;
 import com.blogen.services.PostService;
 import com.blogen.services.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 /**
- * Controller for the Blogen posts page.
+ * Controller for the Blogen "posts" page.
  *
  * @author Cliff
  */
+@Slf4j
 @Controller
 public class PostController {
 
@@ -32,8 +37,7 @@ public class PostController {
         this.categoryService = categoryService;
     }
 
-    //addNewPost - create a new post - POST - /posts
-    // need to check if PostCommand has parentId set in order to determine if this is a child post
+
 
     //delete a post - GET - /posts/{id}/delete
 
@@ -50,14 +54,24 @@ public class PostController {
     public String showAllPosts( Model model ) {
         //todo hardcode userID for now, replace with spring security
         String userName = "johndoe";
+        Long userId = 2L;
         UserCommand userCommand = userService.getUserByUserName( userName );
         PageCommand pageCommand = postService.getAllPostsByUserForPage( userCommand.getId(), 0 );
         List<CategoryCommand> categories = categoryService.getAllCategories();
         pageCommand.setCategories( categories );
         model.addAttribute( "page",pageCommand );
         model.addAttribute( "user",userCommand );
-
+        PostCommand postCommand = new PostCommand();
+        model.addAttribute( "postCommand",postCommand );
         return "posts";
+    }
+
+    //addNewPost - create a new post - POST - /posts
+    // need to check if PostCommand has parentId set in order to determine if this is a child post
+    @PostMapping("/posts")
+    public String addNewPost( @ModelAttribute("postCommand") PostCommand postCommand ) {
+        log.debug( "received new post: \n" + postCommand );
+        return "redirect:posts";
     }
 
 
