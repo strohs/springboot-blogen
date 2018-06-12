5113aabf700c50af042f2337d8ae949f50b0af87
cliff <strohs1@gmail.com>
2018-06-12 15:36:45 -0400

    added code to bootstrap random images from a range of images

--- src/main/java/com/blogen/bootstrap/Bootstrapper.java
+++ src/main/java/com/blogen/bootstrap/Bootstrapper.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 /**
  * Bootstrap the blogen embedded JPA database with data
@@ -31,13 +32,13 @@ public class Bootstrapper implements ApplicationListener<ContextRefreshedEvent> 
     private PostRepository postRepository;
     private RoleService roleService;
 
-    private static final String IMG_SERVICE = "http://lorempixel.com/400/200";
-    private static final String IMG_SERVICE_BUSINESS = "http://lorempixel.com/400/200/business";
-    private static final String IMG_SERVICE_CITY = "http://lorempixel.com/400/200/city";
-    private static final String IMG_SERVICE_ABSTRACT = "http://lorempixel.com/400/200/abstract";
-    private static final String IMG_SERVICE_NATURE = "http://lorempixel.com/400/200/nature";
-    private static final String IMG_SERVICE_FOOD = "http://lorempixel.com/400/200/food";
-    private static final String IMG_SERVICE_GREY = "http://lorempixel.com/g/400/200";
+    private static final String IMG_SERVICE = "https://picsum.photos/300/200";
+    private static final String IMG_SERVICE_BUSINESS = "https://picsum.photos/300/200/?image=1070";
+    private static final String IMG_SERVICE_CITY = "https://picsum.photos/300/200/?image=1071";
+    private static final String IMG_SERVICE_ABSTRACT = "https://picsum.photos/300/200/?image=1072";
+    private static final String IMG_SERVICE_NATURE = "https://picsum.photos/300/200/?image=1073";
+    private static final String IMG_SERVICE_FOOD = "https://picsum.photos/300/200/?image=1074";
+    private static final String IMG_SERVICE_GREY = "https://picsum.photos/g//300/200/?image=1075";
 
     @Autowired
     public Bootstrapper( CategoryRepository categoryRepository,
@@ -147,90 +148,102 @@ public class Bootstrapper implements ApplicationListener<ContextRefreshedEvent> 
         // BUILD POSTS
         //
         //build posts for John - 1 Parent post with 3 child posts
-        PostBuilder pb = new PostBuilder( john, tech, IMG_SERVICE,"Love this tech", "Smart-phones are the greatest invention in the history of mankind",LocalDateTime.of( 2017, 1, 1, 10,11,12 ) );
+        PostBuilder pb = new PostBuilder( john, tech, randomImageUrl(),"Love this tech", "Smart-phones are the greatest invention in the history of mankind",LocalDateTime.of( 2017, 1, 1, 10,11,12 ) );
         Post parent = pb.build();
         //postRepository.save( parent );
-        pb.addChildPost( john,"Love it too", "I wish I could embed the phone into my head", IMG_SERVICE_NATURE, LocalDateTime.of( 2017, 1, 1, 10,12,12 ) );
-        pb.addChildPost( maggie,"Not so fast", "Are they even greater than the Internet?", IMG_SERVICE_FOOD, LocalDateTime.of( 2017, 1, 1, 10,13,12 ));
-        pb.addChildPost( william,"Here today gone tomorrow","They're the greatest for now, but something better will come along", IMG_SERVICE_GREY,LocalDateTime.of( 2017, 1, 1, 10,14,12 ) );
-        pb.addChildPost( william,"No No No","the greatest invention is velcro :)", IMG_SERVICE,LocalDateTime.of( 2017, 1, 1, 10,15,12 ) );
+        pb.addChildPost( john,"Love it too", "I wish I could embed the phone into my head", randomImageUrl(), LocalDateTime.of( 2017, 1, 1, 10,12,12 ) );
+        pb.addChildPost( maggie,"Not so fast", "Are they even greater than the Internet?", randomImageUrl(), LocalDateTime.of( 2017, 1, 1, 10,13,12 ));
+        pb.addChildPost( william,"Here today gone tomorrow","They're the greatest for now, but something better will come along", randomImageUrl(),LocalDateTime.of( 2017, 1, 1, 10,14,12 ) );
+        pb.addChildPost( william,"No No No","the greatest invention is velcro :)", randomImageUrl(),LocalDateTime.of( 2017, 1, 1, 10,15,12 ) );
         postRepository.save( parent );
 
         //
         //build posts for william - 2 parent posts
-        pb = new PostBuilder( william, health,IMG_SERVICE_FOOD,"Started lifting today","Trying to burn off these holiday calories. I hear resistence training is better than running",LocalDateTime.of( 2017, 2, 1, 10,11,12 ) );
+        pb = new PostBuilder( william, health,randomImageUrl(),"Started lifting today","Trying to burn off these holiday calories. I hear resistence training is better than running",LocalDateTime.of( 2017, 2, 1, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
-        pb = new PostBuilder( william, business, "http://lorempixel.com/400/200/business/1","Bulls are on parade", "They stock markets won't stop running higher. When will the bubble burst?",LocalDateTime.of( 2017, 3, 1, 10,11,12 ) );
+        pb = new PostBuilder( william, business, randomImageUrl(),"Bulls are on parade", "They stock markets won't stop running higher. When will the bubble burst?",LocalDateTime.of( 2017, 3, 1, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
         //
         //build posts for maggie - 3 parent posts with 2 child posts each
-        pb = new PostBuilder( maggie, business,"http://lorempixel.com/400/200/business/1","Bitcoin or bust", "Forget about gold, I'm all in on bitcoin",LocalDateTime.of( 2017, 4, 1, 10,11,12 ) );
+        pb = new PostBuilder( maggie, business,randomImageUrl(),"Bitcoin or bust", "Forget about gold, I'm all in on bitcoin",LocalDateTime.of( 2017, 4, 1, 10,11,12 ) );
         parent = pb.build();
-        pb.addChildPost( john,"probably buying it", "I'm game too. I just don't know where to but it from", IMG_SERVICE_NATURE,LocalDateTime.of( 2017, 4, 2, 10,11,12 ) );
-        pb.addChildPost( maggie,"beware the bubble", "If we've waited this long, I fear it's already too late", IMG_SERVICE_BUSINESS,LocalDateTime.of( 2017, 4, 3, 10,11,12 ) );
+        pb.addChildPost( john,"probably buying it", "I'm game too. I just don't know where to but it from", randomImageUrl(),LocalDateTime.of( 2017, 4, 2, 10,11,12 ) );
+        pb.addChildPost( maggie,"beware the bubble", "If we've waited this long, I fear it's already too late", randomImageUrl(),LocalDateTime.of( 2017, 4, 3, 10,11,12 ) );
         postRepository.save( parent );
 
-        pb = new PostBuilder( maggie,health,"http://lorempixel.com/400/200/sports/3","What ever happened to Jazzercise?","It used to be all the rage, now I can't find a single gym that offers it",LocalDateTime.of( 2017, 5, 1, 10,11,12 ) );
+        pb = new PostBuilder( maggie,health,randomImageUrl(),"What ever happened to Jazzercise?","It used to be all the rage, now I can't find a single gym that offers it",LocalDateTime.of( 2017, 5, 1, 10,11,12 ) );
         parent = pb.build();
-        pb.addChildPost( william,"sounds gross", "Jazzercise? Really? What do you do, listen to jazz until you pass out?", IMG_SERVICE_FOOD,LocalDateTime.of( 2017, 5, 2, 10,11,12 ) );
-        pb.addChildPost( william, "on second thought...","I changed my mind. It looks kinda fun. Someone get me leg warmers", IMG_SERVICE_GREY,LocalDateTime.of( 2017, 5, 3, 10,11,12 ) );
+        pb.addChildPost( william,"sounds gross", "Jazzercise? Really? What do you do, listen to jazz until you pass out?", randomImageUrl(),LocalDateTime.of( 2017, 5, 2, 10,11,12 ) );
+        pb.addChildPost( william, "on second thought...","I changed my mind. It looks kinda fun. Someone get me leg warmers", randomImageUrl(),LocalDateTime.of( 2017, 5, 3, 10,11,12 ) );
         postRepository.save( parent );
 
-        pb = new PostBuilder( maggie, webDev,"http://lorempixel.com/400/200/technics/3","Is PHP dead?","Does anyone have stats on PHP usage in the wild?",LocalDateTime.of( 2017, 6, 1, 10,11,12 ) );
+        pb = new PostBuilder( maggie, webDev,randomImageUrl(),"Is PHP dead?","Does anyone have stats on PHP usage in the wild?",LocalDateTime.of( 2017, 6, 1, 10,11,12 ) );
         parent = pb.build();
-        pb.addChildPost( william,"I doubt it", "PHP is everywhere. I'm pretty sure it still powers the internet!", IMG_SERVICE_BUSINESS,LocalDateTime.of( 2017, 6, 2, 10,11,12 ) );
-        pb.addChildPost( admin, "We don't use it here","...anymore. We switched to Kotlin/React, but a lot of companies are still powered by PHP", IMG_SERVICE_ABSTRACT,LocalDateTime.of( 2017, 6, 3, 10,11,12 ) );
+        pb.addChildPost( william,"I doubt it", "PHP is everywhere. I'm pretty sure it still powers the internet!", randomImageUrl(),LocalDateTime.of( 2017, 6, 2, 10,11,12 ) );
+        pb.addChildPost( admin, "We don't use it here","...anymore. We switched to Kotlin/React, but a lot of companies are still powered by PHP", randomImageUrl(),LocalDateTime.of( 2017, 6, 3, 10,11,12 ) );
         postRepository.save( parent );
 
         //
         //Build Posts for elizabeth (10 parent posts for her)
-        pb = new PostBuilder( elizabeth, business, IMG_SERVICE, "Invest now","Market returns are crazy, there is still time to jump on in",LocalDateTime.of( 2017, 1, 1, 10,11,12 ) );
+        pb = new PostBuilder( elizabeth, business, randomImageUrl(), "Invest now","Market returns are crazy, there is still time to jump on in",LocalDateTime.of( 2017, 1, 1, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
-        pb = new PostBuilder( elizabeth, health, IMG_SERVICE_GREY, "Proper Diet trumps all","No matter what excercise you do, just remember you can never out-train a poor diet",LocalDateTime.of( 2017, 1, 2, 10,11,12 ) );
+        pb = new PostBuilder( elizabeth, health, randomImageUrl(), "Proper Diet trumps all","No matter what excercise you do, just remember you can never out-train a poor diet",LocalDateTime.of( 2017, 1, 2, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
-        pb = new PostBuilder( elizabeth, tech, IMG_SERVICE_BUSINESS, "About Alexa","Does anyone own one of these? Is it any good?",LocalDateTime.of( 2017, 1, 3, 10,11,12 ) );
+        pb = new PostBuilder( elizabeth, tech, randomImageUrl(), "About Alexa","Does anyone own one of these? Is it any good?",LocalDateTime.of( 2017, 1, 3, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
-        pb = new PostBuilder( elizabeth, webDev, IMG_SERVICE_BUSINESS, "Bootstrap 4","Hey you all. Would it be worth my time to learn Bootstrap 4?",LocalDateTime.of( 2017, 1, 4, 10,11,12 ) );
+        pb = new PostBuilder( elizabeth, webDev, randomImageUrl(), "Bootstrap 4","Hey you all. Would it be worth my time to learn Bootstrap 4?",LocalDateTime.of( 2017, 1, 4, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
-        pb = new PostBuilder( elizabeth, business, IMG_SERVICE_BUSINESS, "Buying gold","I wanna buy some gold. Can someone point me in the right direction",LocalDateTime.of( 2017, 1, 5, 10,11,12 ) );
+        pb = new PostBuilder( elizabeth, business, randomImageUrl(), "Buying gold","I wanna buy some gold. Can someone point me in the right direction",LocalDateTime.of( 2017, 1, 5, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
-        pb = new PostBuilder( elizabeth, health, IMG_SERVICE_FOOD, "HIIT Training","Forget about running for hours on end. High Intensity Interval Training can give you all the benefits in half the time",LocalDateTime.of( 2017, 1, 6, 10,11,12 ) );
+        pb = new PostBuilder( elizabeth, health, randomImageUrl(), "HIIT Training","Forget about running for hours on end. High Intensity Interval Training can give you all the benefits in half the time",LocalDateTime.of( 2017, 1, 6, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
-        pb = new PostBuilder( elizabeth, tech, IMG_SERVICE_BUSINESS, "Toys that teach Programming","My nephew is showing an interest in programming. Can anyone recommend something for a ten year old?",LocalDateTime.of( 2017, 1, 7, 10,11,12 )  );
+        pb = new PostBuilder( elizabeth, tech, randomImageUrl(), "Toys that teach Programming","My nephew is showing an interest in programming. Can anyone recommend something for a ten year old?",LocalDateTime.of( 2017, 1, 7, 10,11,12 )  );
         parent = pb.build();
         postRepository.save( parent );
 
-        pb = new PostBuilder( elizabeth, webDev, IMG_SERVICE_GREY, "Clojure Script","You guys need to try this http://clojure.org, It saved me hours of web dev work", LocalDateTime.of( 2017, 1, 8, 10,11,12 ) );
+        pb = new PostBuilder( elizabeth, webDev, randomImageUrl(), "Clojure Script","You guys need to try this http://clojure.org, It saved me hours of web dev work", LocalDateTime.of( 2017, 1, 8, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
-        pb = new PostBuilder( elizabeth, tech, IMG_SERVICE, "Samsung Galaxy 8","This phone is the greatest. Nice screen, good battery life, and tons of apps!",LocalDateTime.of( 2017, 1, 9, 10,11,12 ) );
+        pb = new PostBuilder( elizabeth, tech, randomImageUrl(), "Samsung Galaxy 8","This phone is the greatest. Nice screen, good battery life, and tons of apps!",LocalDateTime.of( 2017, 1, 9, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
-        pb = new PostBuilder( elizabeth, webDev, IMG_SERVICE_GREY, "Spring Framework 5","I hear webFlux is all the rage in Spring Framework. Does anyone have first hand experience?",LocalDateTime.of( 2017, 1, 10, 10,11,12 ) );
+        pb = new PostBuilder( elizabeth, webDev, randomImageUrl(), "Spring Framework 5","I hear webFlux is all the rage in Spring Framework. Does anyone have first hand experience?",LocalDateTime.of( 2017, 1, 10, 10,11,12 ) );
         parent = pb.build();
         postRepository.save( parent );
 
 
         //there should now be 26 posts in total - sixteen parent posts and ten child posts
     }
+
+
+    private static int getRandomNumberInRange(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
+    ///quick and dirty random image url
+    private static String randomImageUrl() {
+        return IMG_SERVICE + "/?image=" + getRandomNumberInRange( 1058,1084 );
+    }
+
 
     @Transactional
     @Override
